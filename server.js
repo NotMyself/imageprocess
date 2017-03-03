fc604c9d4e48909c3ddee83237155f40acbf2562
Mike Turro <mturro@mshanken.com>
2017-03-03 15:38:52 -0500

    add error handling to request for image url

--- server.js
+++ server.js
@@ -5,19 +5,20 @@ var express = require('express'),
     app     = express(),
     server;
 
-app.get('/mu-79d650c0-34ab024a-0c011143-8d223b6f', function( req, res ){
-
-        res.send('42');
-
-});
-
 app.get(/d\/(.+)/, function(req, res) {
     var url = config.get('images') + req.params[0];
-    request({ url: url })
-        .on('response', function(image_stream) {
-            transform(image_stream, req.query)
-            .pipe(res);
-        });
+    request(
+        { uri: url }
+        , function (error, response, body) {
+            if(response.statusCode !== 200) {
+                res.status(response.statusCode).send('not-found');
+            }
+        }
+    )
+    .on('response', function(image_stream) {
+        transform(image_stream, req.query)
+        .pipe(res);
+    });
 });
 
 app.listen(config.get('port'), function(){
